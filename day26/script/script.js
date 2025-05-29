b116f4795179517eb50ec35b84996e48f249143c
biwakho <yabe.toshiharu@shiga-web.or.jp>
2025-05-29 09:56:17 +0900

    Add files via upload

---
+++ day26/script/script.js
@@ -0,0 +1,17 @@
+$(function() {
+	$('section').fadeIn(1000); //1秒かけてフェードイン！
+    $('footer').fadeIn(1000);
+});
+
+
+// jQuery(".aboutbg").on({
+//     click:function(){
+//     $(this).css("background-color","orange");
+//     },
+// });
+// jQuery(".homebg").on({
+//     click:function(){
+//     $(this).css("background-color","orange");
+//     },
+// });
+
